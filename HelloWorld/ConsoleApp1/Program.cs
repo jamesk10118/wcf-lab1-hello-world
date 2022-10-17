9412113c36440727d3b7a1c6b136e0360b3d9782
jamesk10118 <jamesk10118@gmail.com>
2022-10-17 14:02:43 -0400

    Added stuff

---
+++ HelloWorld/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            var res = client.GetData(100);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
